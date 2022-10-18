2f07636171f1461339242b66743e44d1f925514d
swineethamurthy <swineethamurthy@gmail.com>
2022-10-18 19:15:38 +0530

    chore: added back logo folder

---
+++ src/components/atoms/logo/Logo.Atom.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import { Link } from 'gatsby';
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
+import React from 'react';
+import { StandardFC } from '../../../types/libs/react.lib';
+
+interface LogoProps {
+  gatsbyImageData: IGatsbyImageData;
+  options?: Record<string, any>;
+}
+
+const Logo: StandardFC<LogoProps> = ({ gatsbyImageData, options }) => (
+  <GatsbyImage
+    image={gatsbyImageData}
+    alt="Logo"
+    className={clsx('h-full', 'w-auto')}
+    {...{ objectFit: 'contain', objectPosition: 'left', ...options }}
+  />
+);
+
+export default Logo;
