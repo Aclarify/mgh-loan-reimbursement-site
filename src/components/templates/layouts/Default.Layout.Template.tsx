0ba73b4efd8044d222adf11e577ff84e20ca8b5a
swineethamurthy <swineethamurthy@gmail.com>
2022-12-01 21:50:41 +0530

    feat: added cookie consent

--- src/components/templates/layouts/Default.Layout.Template.tsx
+++ src/components/templates/layouts/Default.Layout.Template.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { StandardFC } from '../../../types/libs/react.lib';
 import Footer from '../../organisms/footer/Footer.Organism';
 import NavBar from '../../organisms/navBar/NavBar.Organism';
+import CookieConsent from 'react-cookie-consent';
 
 interface DefaultLayoutProps {}
 
@@ -11,6 +12,27 @@ const DefaultLayout: StandardFC<DefaultLayoutProps> = ({ children }) => {
     <>
       <NavBar />
       <main className={clsx('h-full', 'w-full', 'relative')}>{children}</main>
+      <CookieConsent
+        location="bottom"
+        buttonText="I understand"
+        cookieName="cookieConsent"
+        style={{ background: '#2B373B' }}
+        buttonStyle={{
+          background: '#206B9E',
+          border: 'none',
+          borderRadius: '4px',
+          fontFamily: 'inter',
+          color: '#fff',
+          fontSize: '14px',
+          fontWeight: 'bold',
+        }}
+        expires={150}
+      >
+        <span className="font-inter text-white text-base text-normal">
+          {' '}
+          This website might use cookies to enhance the user experience.{' '}
+        </span>
+      </CookieConsent>
       <Footer />
     </>
   );
