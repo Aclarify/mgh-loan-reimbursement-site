354089e88501550bf23648f8b31dacfbbfefaf04
swineethamurthy <swineethamurthy@gmail.com>
2022-12-05 18:54:29 +0530

    fix: completed sorting issues

--- src/components/molecules/formcontrols/ComboBox.Molecule.tsx
+++ src/components/molecules/formcontrols/ComboBox.Molecule.tsx
@@ -19,7 +19,6 @@ const ComboBox: React.FC<Props> = ({
   onChange,
   showError,
 }) => {
-  options.sort((a, b) => (a.label > b.label ? 1 : -1));
   const [query, setQuery] = useState('');
   const [showOptions, setShowOptions] = useState(false);
   const wrapperRef = useRef(null);
